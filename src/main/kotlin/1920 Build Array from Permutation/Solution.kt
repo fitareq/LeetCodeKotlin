4bc2dcab85a72ade856ed26c06362dddc094101a
Forhadul Islam <fitareq@gmail.com>
2022-04-26 14:42:00 +0600

    1920 build array from permutation

--- src/main/kotlin/1920 Build Array from Permutation/Solution.kt
+++ src/main/kotlin/1920 Build Array from Permutation/Solution.kt
@@ -1,12 +1,20 @@
 package `1920 Build Array from Permutation`
 
 class Solution {
-    fun buildArray(nums: IntArray): IntArray {
+  /*  fun buildArray(nums: IntArray): IntArray {
         val res  = MutableList(nums.size){0}
         for(i in nums.indices)
         {
             res[i] = nums[nums[i]]
         }
         return res.toIntArray();
+    }*/
+    fun buildArray(nums: IntArray): IntArray {
+        val res  = IntArray(nums.size)
+        for(i in nums.indices)
+        {
+            res[i] = nums[nums[i]]
+        }
+        return res;
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 import `1920 Build Array from Permutation`.Solution
 
-fun main(args: Array<String>) {
-   val s: Solution
+fun main() {
+   val s = Solution()
+   val arr = intArrayOf(0,2,1,5,3,4)
+
+   val res = s.buildArray(arr)
+   for (i in res)
+      print("$i ")
 }
 
